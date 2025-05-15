0c610c816c9b414733520ef800f29fa2958d8561
Raksivaa <kazulaw69@gmail.com>
2025-05-15 08:19:03 +0545

    Add two buttons

--- clothing_website/src/component/NewCollection.jsx
+++ clothing_website/src/component/NewCollection.jsx
@@ -1,9 +1,11 @@
 import { MoveRight } from "lucide-react";
+import { ChevronRight } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 
 export default function NewCollection() {
   return (
     <>
-      <div className="grid grid-cols-3 ml-14 mt-14 h-auto gap-20">
+      <div className="grid grid-cols-3 ml-14 mt-14 h-auto gap-2">
         <div className="font-beatrix-deck-trial font-extrabold text-[48px] leading-none">
           <div className="mb-3">
             <h2 className="text-black drop-shadow-[4px_4px_0px_rgba(0,0,0,0.2)]">
@@ -19,8 +21,8 @@ export default function NewCollection() {
             <p>2024</p>
           </div>
 
-          <div className="mt-40 w-80">
-            <button className="btn w-full rounded-none bg-[#D9D9D9] flex flex-row justify-between">
+          <div className="mt-40 flex justify-between">
+            <button className="btn w-80 rounded-none bg-[#D9D9D9] flex flex-row justify-between">
               <span className="text-[16px] font-beatrix-deck-trial ">
                 Go to Shop
               </span>
@@ -28,10 +30,18 @@ export default function NewCollection() {
                 <MoveRight size={50} opacity={0.8} strokeWidth={1} />
               </span>
             </button>
+            <div className="space-x-2 flex">
+              <button className="inline-flex cursor-pointer rounded-none btn w-fit p-2 min-w-0 min-h-0 hover:brightness-95">
+                <ChevronLeft size={28} strokeWidth={1.9} />
+              </button>
+              <button className="inline-flex cursor-pointer rounded-none btn w-fit p-2 min-w-0 hover:brightness-95">
+                <ChevronRight size={28} strokeWidth={1.9} />
+              </button>
+            </div>
           </div>
         </div>
 
-        <div className="">
+        <div>
           <img
             src="./newCollectionPics/newCollection_pic1.png"
             alt="Trousers"
