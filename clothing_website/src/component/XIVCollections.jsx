2ca22563cda5b89dfbd37c2bb9532fe2def58e03
Raksivaa <kazulaw69@gmail.com>
2025-05-18 11:14:10 +0545

    Remove unnecessary div

--- clothing_website/src/component/XIVCollections.jsx
+++ clothing_website/src/component/XIVCollections.jsx
@@ -3,13 +3,9 @@ export default function XIVCollections() {
     <>
       <section className="flex flex-col justify-evenly items-start p-12">
         <div className="font-beatrix-deck-trial font-extrabold text-[48px] leading-none space-x-4 p-1 flex flex-col">
-          <div className="flex flex-row items-center justify-between w-full">
-            <div className="flex flex-col">
-              <p>XIV</p>
-              <p>COLLECTIONS</p>
-              <p>23-24</p>
-            </div>
-          </div>
+          <p>XIV</p>
+          <p>COLLECTIONS</p>
+          <p>23-24</p>
         </div>
       </section>
     </>
